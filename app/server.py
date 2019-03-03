2226e136e479c0cf430b5089cfdd3dc45451a013
kakpu <44431969+kakpu@users.noreply.github.com>
2019-03-03 22:27:22 +0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/open?id=1JEkxH2mFpcpW2xgX7298KQNTY2af39wx'
+export_file_url = 'https://drive.google.com/open?id=11_rdhnpazM8yHyNrPRRRNEYarbs8gpOl'
 export_file_name = 'export.pkl'
 
 classes = ['asagakita', 'amachan']
